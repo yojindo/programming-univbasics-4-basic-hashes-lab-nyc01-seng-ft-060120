c718037de2a9f2ba49839ed62e8066690c6ef6a8
Jake Kwak <jakejaekwak@gmail.com>
2020-05-12 18:59:58 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -8,10 +8,10 @@ end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
-  hash = {name : 'Grace Hopper'
+  hash = {name: 'Grace Hopper'
 end
 
 
 def id_hash_generator(number)
-  # return a hash with a key :id assigned to the provided number
+  hash = {key: number}
 end
